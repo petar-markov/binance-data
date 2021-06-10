58865a9479e4e769b15af5e03c5daf57adaae14f
Petar Markov Tsvetkov <ptsvetkov01@gmail.com>
2021-06-10 12:58:27 +0300

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -4,13 +4,12 @@ from itertools import combinations
 from typing import Type
 from generic_helpers import gen_dates_interval, last_day_of_month
 from returns_statistics import sharpe_ratio, sortino_ratio
-from math_helpers import correlation
+from math_helpers import correlation, rankdata
 
 def get_monthly_returns_and_statistics(start: str, 
                         end: str, 
                         file_name:str = '', 
-                        save:bool = False, 
-                        output_file_name:str = 'monthly_returns.json') -> dict:
+                        save:bool = False) -> dict:
     """
     params:
         start - begin of interval for 1M returns to compute, in format YYYY-MM-DD
@@ -45,10 +44,13 @@ def get_monthly_returns_and_statistics(start: str,
         raise ValueError('Invalid start date provided. Start date must be >= 2019-12-31!')
 
     if end > datetime.date(2021, 5, 31):
-        raise ValueError('Invalid end date provided. Start date must be <= 2021-05-31!')
+        raise ValueError('Invalid end date provided. End date must be <= 2021-05-31!')
     
     month_returns = {}
+    market_cap_series = {}
+    rank_corr_coef_results = {}
     all_correlations = {}
+    all_stats = {}
     
     for d in gen_dates_interval(start, end, datetime.timedelta(days=1)):
         prev_month_end = d.strftime('%Y-%m-%d')
@@ -71,15 +73,44 @@ def get_monthly_returns_and_statistics(start: str,
             # Because of how we iterate those should be always in ascending order
             # TODO: check for this...
             month_returns[ccy].append(current_return)
-            
+
+            # Sharpe and Sortino ratios calculated in here
             try:
                 sharpe = sharpe_ratio(month_returns[ccy], len(month_returns[ccy]))
-                sortino = sortino_ratio(month_returns[ccy], len(month_returns[ccy]))
             except AssertionError:
                 sharpe = 0
+                
+            try:
+                sortino = sortino_ratio(month_returns[ccy], len(month_returns[ccy]))
+            except AssertionError:
                 sortino = 0
-            print(f'1M Return for: {prev_month_end}, {cur_month_end}, {ccy}, {current_return}, Sharpe Ratio = {sharpe}, Sortino Ratio = {sortino}')
-        
+
+            # Rank correlation starting in here
+            # Custom functions used, but the final results verified against
+            # scipy.stats.spearmanr and give pretty much the same results
+            if ccy not in market_cap_series:
+                market_cap_series[ccy] = []
+            if ccy not in rank_corr_coef_results:
+                rank_corr_coef_results[ccy] = []
+            
+            mk = float(ds.get(ccy).get(cur_month_end)[-1])
+            market_cap_series[ccy].append(mk)
+
+            ranked_returns = rankdata(month_returns[ccy])
+            ranked_market_cap = rankdata(market_cap_series[ccy])
+            try:
+                rank_corr_coef = correlation(ranked_returns, ranked_market_cap)
+            except AssertionError:
+                rank_corr_coef = 0
+            
+            rank_corr_coef_results[ccy].append(rank_corr_coef)
+            
+            # Add all the monthly data for the current crypto in a mutual dataset to be returned later
+            all_stats[cur_month_end] = [ccy, current_return, sharpe, sortino, rank_corr_coef]
+            print(f'1M Return for: {prev_month_end}, {cur_month_end}, {ccy}, {current_return}, Sharpe Ratio = {sharpe}, Sortino Ratio = {sortino}, Rank corr. coef (market cap vs. return) = {rank_corr_coef}')
+
+
+        # Correlation between the pairs' returns starting in here        
         pairs = [",".join(map(str, comb)) for comb in combinations(month_returns.keys(), 2)]
         if pairs:
             all_correlations[cur_month_end] = []
@@ -96,12 +127,12 @@ def get_monthly_returns_and_statistics(start: str,
         print(f'Pair with max correlation as of {cur_month_end}: {sorted(all_correlations[cur_month_end], key = lambda x:x[1], reverse = True)[0]}')
 
     # If save option is enabled all results are saved to the FS
-    # Even though the 2 files are JSON format, by definition the whole
-    # output file will not be a valid JSON, because we dumpt both in the same file.
-    # If this is processed/worked further, this has to be worked out.
+    # 2 files are output, both in JSON format,
+    # first one has all monthly based statistics and the second one has all the
+    # pairs' returns correlations
     if save:
-        with open(output_file_name, 'w') as f:
-            json.dump(month_returns, f)
-            json.dump(all_correlations, f)
+        with open('all_stats.json', 'w') as f0, open('pairs_returns_correlations.json', 'w') as f1:
+            json.dump(all_stats, f0)
+            json.dump(all_correlations, f1)
 
-    return month_returns, all_correlations
+    return all_stats, all_correlations
